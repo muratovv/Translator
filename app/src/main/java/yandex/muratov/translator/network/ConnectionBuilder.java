d6c3eba1a9b9399d7f0a944c3704a362a767e669
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-21 06:33:30 +0300

    add OkHttpClient features: - cache - deferred requests

--- app/src/main/java/yandex/muratov/translator/network/ConnectionBuilder.java
+++ app/src/main/java/yandex/muratov/translator/network/ConnectionBuilder.java
@@ -2,24 +2,34 @@ package yandex.muratov.translator.network;
 
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
+
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ConnectionBuilder {
+    private static int CACHE_SIZE_MB = 10;
+
     private String apiKey;
     private String baseUrl;
     private OkHttpClient sharedClient;
 
     private Retrofit.Builder commonRetrofitBuilder = new Retrofit.Builder();
-    private OkHttpClient client;
 
-    public ConnectionBuilder(String apiKey, String baseUrl, OkHttpClient sharedClient) {
+    public ConnectionBuilder(File cahceDir, String apiKey, String baseUrl) {
         this.apiKey = apiKey;
         this.baseUrl = baseUrl;
-        this.sharedClient = sharedClient;
-        this.client = sharedClient;
+        makeOkHttpClient(cahceDir);
         makeBuilder();
+    }
+
+    private void makeOkHttpClient(File cacheDir) {
+        sharedClient = new OkHttpClient.Builder()
+                .addInterceptor(new DeferredRequestInterceptor())
+                .cache(new Cache(cacheDir, CACHE_SIZE_MB * 1024 * 1024))
+                .build();
     }
 
     private void makeBuilder() {
@@ -39,6 +49,7 @@ public class ConnectionBuilder {
     }
 
     public OkHttpClient getClient() {
-        return client;
+        return sharedClient;
     }
+
 }
---
+++ app/src/main/java/yandex/muratov/translator/network/DeferredRequestInterceptor.java
@@ -0,0 +1,29 @@
+package yandex.muratov.translator.network;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DeferredRequestInterceptor implements Interceptor {
+
+    private double deferredSeconds = 1.;
+    private long deferredMillis = (Double.valueOf(deferredSeconds * 1000)).longValue();
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+
+        waitBeforeRequest();
+
+        return chain.proceed(request);
+    }
+
+    private void waitBeforeRequest() {
+        try {
+            Thread.sleep(deferredMillis);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
