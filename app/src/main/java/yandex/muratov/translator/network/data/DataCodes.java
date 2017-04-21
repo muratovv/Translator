7c5d048991defad2d027e9c164ca6cc053e2e439
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-21 06:53:01 +0300

    Redesign network errors: Codes represents as R.string.<id>

--- app/src/main/java/yandex/muratov/translator/network/data/DataCodes.java
+++ app/src/main/java/yandex/muratov/translator/network/data/DataCodes.java
@@ -4,10 +4,12 @@ package yandex.muratov.translator.network.data;
 import java.util.HashMap;
 import java.util.Map;
 
+import yandex.muratov.translator.R;
+
 public class DataCodes {
 
     public static final int VALID_CODE = 200;
-    public static final String NO_ERROR = "";
+    public static final Integer NO_ERROR = 0;
 
     public static boolean isValid(TranslateAnswer answer) {
         return answer != null && answer.getCode() == VALID_CODE;
@@ -17,43 +19,40 @@ public class DataCodes {
         return answer != null && answer.getCode() == VALID_CODE;
     }
 
-    public static String getError(TranslateAnswer answer) {
+    public static Integer getResourceByError(TranslateAnswer answer) {
         if (isValid(answer))
             return NO_ERROR;
-        String s = allDataErrors.get(answer.getCode());
-        if (s == null) {
-            return addResourcePrefix("unknown_error");
+        Integer code = allDataErrors.get(answer.getCode());
+        if (code == null) {
+            return R.string.translation_api_error_unknown_network_error;
         } else {
-            return s;
+            return code;
         }
     }
 
-    public static String getError(DictionaryAnswer answer) {
+    public static Integer getResourceByError(DictionaryAnswer answer) {
         if (isValid(answer))
             return NO_ERROR;
-        String s = allDataErrors.get(answer.getCode());
-        if (s == null) {
-            return addResourcePrefix("unknown_error");
+        Integer code = allDataErrors.get(answer.getCode());
+        if (code == null) {
+            return R.string.translation_api_error_unknown_network_error;
         } else {
-            return s;
+            return code;
         }
     }
 
-    public static Map<Integer, String> allDataErrors = new HashMap<Integer, String>() {
-        private HashMap<Integer, String> inflate() {
-            this.put(401, addResourcePrefix("invalid_api_key"));
-            this.put(402, addResourcePrefix("blocked_api_key"));
-            this.put(404, addResourcePrefix("exceed_daily_limit"));
-            this.put(413, addResourcePrefix("exceed_maximum_text_size"));
-            this.put(422, addResourcePrefix("text_cannot_be_translated"));
-            this.put(501, addResourcePrefix("translation_direction_not_supported"));
+    public static Map<Integer, Integer> allDataErrors = new HashMap<Integer, Integer>() {
+        private HashMap<Integer, Integer> inflate() {
+
+            this.put(401, R.string.translation_api_error_invalid_api_key);
+            this.put(402, R.string.translation_api_error_blocked_api_key);
+            this.put(404, R.string.translation_api_error_exceed_daily_limit);
+            this.put(413, R.string.translation_api_error_exceed_maximum_text_size);
+            this.put(422, R.string.translation_api_error_text_cannot_be_translated);
+            this.put(501, R.string.translation_api_error_translation_direction_not_supported);
             return this;
         }
     }.inflate();
 
-    private static String addResourcePrefix(String text) {
-        String prefix = "R.string.translation_api_error_";
-        return prefix + text;
-    }
 
 }
