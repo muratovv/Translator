1fdd6765a6e8d0197174e4b3008afb116fa4a5d5
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-22 04:53:53 +0300

    Add request logger

--- app/src/main/java/yandex/muratov/translator/network/ConnectionBuilder.java
+++ app/src/main/java/yandex/muratov/translator/network/ConnectionBuilder.java
@@ -1,13 +1,17 @@
 package yandex.muratov.translator.network;
 
 import com.google.gson.GsonBuilder;
+import com.ihsanbal.logging.Level;
+import com.ihsanbal.logging.LoggingInterceptor;
 
 import java.io.File;
 
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.internal.platform.Platform;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import yandex.muratov.translator.BuildConfig;
 
 public class ConnectionBuilder {
     private static int CACHE_SIZE_MB = 1;
@@ -23,6 +27,13 @@ public class ConnectionBuilder {
 
     private void makeOkHttpClient(File cacheDir) {
         okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new LoggingInterceptor.Builder()
+                        .loggable(BuildConfig.DEBUG)
+                        .setLevel(Level.BASIC)
+                        .log(Platform.INFO)
+                        .request("request")
+                        .response("response")
+                        .build())
                 .addInterceptor(new DeferredRequestInterceptor())
                 .cache(new Cache(cacheDir, CACHE_SIZE_MB * 1024 * 1024))
                 .build();
