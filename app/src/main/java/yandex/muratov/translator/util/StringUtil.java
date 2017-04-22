4e45b1051eaac2cb019b74d521fd27aae088202a
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-22 04:54:49 +0300

    Fix join bug

--- app/src/main/java/yandex/muratov/translator/util/StringUtil.java
+++ app/src/main/java/yandex/muratov/translator/util/StringUtil.java
@@ -11,7 +11,7 @@ public class StringUtil {
             builder.append(collection.get(pos))
                     .append(separator);
         }
-        if(size > 1){
+        if(size > 0){
             builder.append(collection.get(size - 1));
         }
         return builder.toString();
