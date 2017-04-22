ddeb27c3af2a0b2dfdece3fff9ad44029cd7eb28
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-22 04:40:09 +0300

    Fix non cancelled requests

--- app/src/main/java/yandex/muratov/translator/network/CallHolder.java
+++ app/src/main/java/yandex/muratov/translator/network/CallHolder.java
@@ -3,19 +3,11 @@ package yandex.muratov.translator.network;
 import retrofit2.Call;
 import retrofit2.Callback;
 
-/**
- * @author @muratovv
- * @date 20.04.17
- */
 class CallHolder<T> {
     private Call<T> call;
 
     public void dropPreviousCall() {
         if (call != null) {
-            if (call.isExecuted()) {
-                call = null;
-                return;
-            }
             if (!call.isCanceled()) {
                 call.cancel();
                 call = null;
